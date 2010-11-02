6874e383e5432b54a82d3d43b4c39a3429db925a
Raymond Niemeir <rayn@mb1.home>
2010-11-01 21:03:47 -0400

    Initial commit

---
+++ mkphotohtml.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Emit on stdout a simple index.html to show photos in $1 or "."
+#
+
+  # Verbose defaults unless set in env
+  v=${v:-"t"}
+  dir=${1:-"."}
+  [[ $v ]] && echo dir=$dir
+  
+cat <<END
+<html>
+<head>
+  <title>Directory $dir</title>
+</head>
+<body>
+<h2>$dir</h2>
+END
+
+  for f in $dir/*.jpg
+  do
+  cat <<END
+<a href="$f">
+<img src="$f" width="25%">
+</a><br />
+END
+  done
+
+cat <<END
+</body>
+</html>
+END
